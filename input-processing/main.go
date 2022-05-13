67511039dbaa9e90fb024cc9843173255fa144c5
ENDAVA\mpezzolano <marcelo.pezzolano@bango.com>
2022-05-13 16:11:51 -0300

    02- input-procesing

--- input-processing/main.go
+++ input-processing/main.go
@@ -3,15 +3,47 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("SP// Backend Developer Test - Input Processing")
 	fmt.Println()
 
-	// Read STDIN into a new buffered reader
-	reader := bufio.NewReader(os.Stdin)
+	ch := make(chan string)
 
-	// TODO: Look for lines in the STDIN reader that contain "error" and output them.
+	go SearchForErrors(ch, os.Stdin)
+
+	for message := range ch {
+		fmt.Println(message)
+
+	}
+
+}
+func SearchForErrors(c chan string, r io.Reader) {
+	// Scanner was recommended by the standard library
+	// However it has a max token size of 64*1024
+	// Since we must support larger inputs Reader must be used instead.
+	defer close(c)
+
+	// Don't read anything bigger than 1GB
+	// Can be tweaked based on performance needs.
+	reader := bufio.NewReaderSize(r, 64*64*64*4096)
+
+	for {
+		token, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				c <- ""
+				break
+			}
+			fmt.Println(err)
+		}
+
+		if strings.Contains(token, errorMsg) {
+			c <- token
+		}
+	}
 }
