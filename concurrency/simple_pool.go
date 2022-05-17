0774d75735b0db7fd0805b621c6b4ed9fbd6fa14
ENDAVA\mpezzolano <marcelo.pezzolano@bango.com>
2022-05-17 10:11:23 -0300

    03 simple pool

--- concurrency/simple_pool.go
+++ concurrency/simple_pool.go
@@ -2,6 +2,8 @@
 // bit more complex.
 package concurrency
 
+import "fmt"
+
 // SimplePool is a simple worker pool that does not support cancellation or
 // closing. All functions are safe to call from multiple goroutines.
 type SimplePool interface {
@@ -15,5 +17,32 @@ type SimplePool interface {
 // concurrent tasks to run at any one time. maxConcurrent must be greater than
 // zero.
 func NewSimplePool(maxConcurrent int) SimplePool {
-	panic("TODO")
+	var sp SimplePool
+	jobs := make(chan func())
+	sp = &JobPool{maxConcurrent, jobs}
+
+	// Create workers and jobs channel
+	for i := 0; i < maxConcurrent; i++ {
+		go Worker(i, jobs)
+	}
+
+	return sp
+}
+
+// Generic worker with some added text to verify things are working
+func Worker(id int, jobs <-chan func()) {
+	for job := range jobs {
+		fmt.Printf("worker %v starting sleep\n", id)
+		job()
+		fmt.Printf("worker %v awake\n", id)
+	}
+}
+
+type JobPool struct {
+	maxWorkers int
+	jobs       chan func()
+}
+
+func (jp *JobPool) Submit(task func()) {
+	jp.jobs <- task
 }
